14d4e1352b4eda121d3f51d52b3a13c297868d06
seikichin <seikichin@gmail.com>
2026-09-14 23:08:02 +0800

    feat(vrm): port VRM v2 fixed-shape maze + dumpmaze/vrmcheck + d/fixedtest
    new: include/vrm.h, std/room/{vrm_base,vrm_dungeon,vrm_fixed,vrm_fixed_room}.lpc, cmds/wiz/{vrmcheck,dumpmaze}.lpc, d/fixedtest/*
    
    rewrite: std/room/{vrm_server,vrm_server2}.lpc, obj/dungeon/{maze,standardmaze,halloween}.lpc
    
    fix: dumpmaze arg parsing via explode (driver sscanf returns 0 when args<formats); add query_maze_built() in vrm_base; guard query_maze_dump_lines via function_exists
    
    verified: all 18 lpc compile clean with remote lpcc (exit=0, 0 errors)

---
+++ include/vrm.h
@@ -0,0 +1,16 @@
+// vrm.h - 虚拟随机迷宫（Virtual Random Maze）公共宏定义
+#ifndef __VRM_H__
+#define __VRM_H__
+
+#define UP   32
+#define D    16
+#define N    8
+#define S    4
+#define W    2
+#define E    1
+#define ALL  63
+
+#define MAX_LONG     100
+#define VALID_LEAVES 3
+
+#endif
